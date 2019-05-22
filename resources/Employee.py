46754a561fac908b58e0daa2cf167a99397ef682
Vijay Karthik P <vjykrthk@gmail.com>
2019-05-22 20:39:09 +0530

    completed adding swagger ui to the api endpoint

--- resources/Employee.py
+++ resources/Employee.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request
+from flask import request
 from flask_restful import Resource
 
 from external_services.dummy_client import DummyClient
@@ -8,17 +8,24 @@ dummy_client = DummyClient()
 
 class Employee(Resource):
     def get(self):
-        employees = dummy_client.get_employees()
-        return {
-            'success': True,
-            'data': employees
-        }, 200
-
+        res = dummy_client.get_employees()
+        employees = res.json()
+        try:
+            return {
+                       'success': True,
+                       'data': employees
+                   }, 200
+        except Exception as e:
+            return {
+                       'success': False,
+                       'error': str(res.content)
+                   }, 400
 
     def post(self):
-        post_data = request.get_json(force=True)
-        response = dummy_client.create_employee(post_data)
-        return {
-            'success': True,
-            'data': response
-        }, 201
+        try:
+            post_data = request.get_json(force=True)
+            response = dummy_client.create_employee(post_data)
+            employee = response.json()
+            return {'success': True, 'data': employee}, 201
+        except Exception as e:
+            return {'success': False, 'error': str(response.content)}, 400
